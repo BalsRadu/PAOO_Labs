e14ca14b049d0ca21d6ed9b1b1585c19f57084e2
BalsRadu <bals.radu@gmail.com>
2023-10-09 21:16:21 +0300

    Small changes

--- cmake_tutorial/Step1/tutorial.cxx
+++ cmake_tutorial/Step1/tutorial.cxx
@@ -9,11 +9,13 @@ int main(int argc, char* argv[])
 {
   if (argc < 2) {
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-          << Tutorial_VERSION_MINOR << std::endl;
+              << Tutorial_VERSION_MINOR << std::endl;
+              
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
 
+  // convert input to double
   const double inputValue = std::stod(argv[1]);
 
   // calculate square root
