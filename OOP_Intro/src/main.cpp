e0a30b98685c156912a0f56cf5a870d67f0e6ad4
BalsRadu <bals.radu@gmail.com>
2023-10-23 15:42:18 +0300

    Added bugged version

--- OOP_Intro/src/main.cpp
+++ OOP_Intro/src/main.cpp
@@ -7,6 +7,10 @@
 int main() {
     char * s = new char[MAX_LEN];
     memcpy(s, "Sal", MAX_LEN);
+    Class1 c1(1, 2.0, s);
+    {
+        Class1 y = c1;
+    }
     Class1 c2 = std::move(Class1(2, 2.0, s));
     return 0;
 }
